83bec5501dfe77268143da761dd49a51485b6f26
ushisha <usisha.s@gmail.com>
2023-04-13 14:40:44 +0100

    Edit about page

--- pages/about.js
+++ pages/about.js
@@ -14,20 +14,22 @@ export default function AboutPage() {
           How it started...
         </h3>
         <p className="text-center md:text-left mb-3">
-          Ikuko started Yoga in 2013 at Indaba studios in London, practising
-          Vinyasa Yoga, Ashtanga Yoga and Iyengar Yoga under the guidance of
+          Ikuko started her Yoga journey in 2013 at Triyoga Studios in London,
+          practising Vinyasa, Ashtanga and Iyengar under the guidance of
           different yoga instructors. In 2014, she went to New York to accompany
-          her friend's teacher training trip and visited Dharma Yoga New York
-          Center. She took her first class with Sri Dharma Mittra, and she
-          started her new life the same day. She found her Guru, found Yoga.
+          her friend's teacher training trip and visited Dharma Yoga Center in
+          New York. She took her first class with Sri Dharma Mittra and started
+          her new life the same day. She found her Guru, found Yoga.
           <br />
           <br />
           She is passionate about the philosophy of Yoga and the yogic way of
           life. She loves sharing the central teachings of the philosophy and
-          wisdom she has learnt. She is reminded every day of the power of Yoga:
-          on her body, as Yoga has helped her remain strong and build more
-          strength, and on her well-being and her mind, as Yoga gives us the
-          tools to find the peace we need when we are so busy off the mat.
+          wisdom she has learnt.
+          <br />
+          <br />
+          She is reminded every day of the power of Yoga; Yoga helps her remain
+          strong, builds more strength, and brings peace to her mind despite our
+          busy lives off the mat.
           <br />
           <br />
           This blog is about her findings and passion for Yoga she wants to
@@ -43,7 +45,11 @@ export default function AboutPage() {
           />
         </div>
 
-        <p className="text-center md:text-left font-bold">Version 1.1.0</p>
+        <p className="text-center md:text-left font-bold">
+          Created with Next.js
+          <br />
+          Version 1.1.0
+        </p>
       </div>
     </Layout>
   )
